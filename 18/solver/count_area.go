507430e5f76a1144705d0315f678d756d1abb245
mg-hato <milosgolub97@gmail.com>
2023-06-26 17:21:10 +0100

    F-18: Remove printing

--- 18/solver/count_area.go
+++ 18/solver/count_area.go
@@ -9,7 +9,7 @@ func CountAreaOfDropletSurfaces(envelope m.SolverInput) (int, error) {
 	droplets := c.CreateSet(envelope.Get(), c.Identity[m.Droplet])
 
 	area := 0
-	for droplet, _ := range droplets {
+	for droplet := range droplets {
 		area += 6 - c.Count(
 			droplet.GetAllNeighbours(),
 			func(d m.Droplet) bool { return droplets[d] },
